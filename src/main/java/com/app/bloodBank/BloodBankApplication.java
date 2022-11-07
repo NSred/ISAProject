2586232e886d8d7a93e17cbcd533bccba2ace823
Katarina Artukov <96656266+Vokutra@users.noreply.github.com>
2022-11-07 22:30:48 +0100

    Add cors configuration

--- src/main/java/com/app/bloodBank/BloodBankApplication.java
+++ src/main/java/com/app/bloodBank/BloodBankApplication.java
@@ -22,5 +22,4 @@ public class BloodBankApplication {
 	      return new Docket(DocumentationType.SWAGGER_2).select()
 	         .apis(RequestHandlerSelectors.basePackage("com.app.bloodBank")).build();
 	   }
-
 }
